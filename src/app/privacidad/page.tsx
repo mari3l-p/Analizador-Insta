55570571208269be7bf36dde0c94faf00fd55868
Mariel <mari3lpalacio@gmail.com>
2025-10-12 21:03:56 -0600

    Fix unescaped quotes

--- src/app/privacidad/page.tsx
+++ src/app/privacidad/page.tsx
@@ -19,7 +19,7 @@ return (
             <div>
                 <h3 className="font-medium text-lg text-center">¿Cómo Funciona?</h3>
                 <ol className="w-2xs mx-auto list-decimal marker:text-white steps">
-                    <li className="gray-text"><span className="white-text">Todo es Local:</span> El análisis de tu archivo ZIP ocurre directamente en tu navegador lo que se conoce como "procesamiento del lado del cliente".</li>
+                    <li className="gray-text"><span className="white-text">Todo es Local:</span> El análisis de tu archivo ZIP ocurre directamente en tu navegador lo que se conoce como procesamiento del lado del cliente.</li>
                     <li className="gray-text"><span className="white-text">No hay Servidores Involucrados:</span> Cuando subes tu archivo, nuestro código lo lee y lo procesa localmente en tu equipo. No se envía a ningún servidor en internet.</li>
                     <li className="gray-text"><span className="white-text">La Información se Desvanece:</span> Los resultados se generan y se te muestran a ti, y a nadie más. Una vez que cierras la pestaña del navegador, toda la información y los resultados se eliminan por completo.</li>
                 </ol>

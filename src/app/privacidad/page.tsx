4e993185055034956d852288f8f0f517fef40e3f
Mariel <mari3lpalacio@gmail.com>
2025-10-12 21:01:32 -0600

    Fix unescaped quotes in privacidad page

--- src/app/privacidad/page.tsx
+++ src/app/privacidad/page.tsx
@@ -19,7 +19,7 @@ return (
             <div>
                 <h3 className="font-medium text-lg text-center">¿Cómo Funciona?</h3>
                 <ol className="w-2xs mx-auto list-decimal marker:text-white steps">
-                    <li className="gray-text"><span className="white-text">Todo es Local:</span> El análisis de tu archivo ZIP ocurre directamente en tu navegador (lo que se conoce como "procesamiento del lado del cliente").</li>
+                    <li className="gray-text"><span className="white-text">Todo es Local:</span> El análisis de tu archivo ZIP ocurre directamente en tu navegador lo que se conoce como "procesamiento del lado del cliente".</li>
                     <li className="gray-text"><span className="white-text">No hay Servidores Involucrados:</span> Cuando subes tu archivo, nuestro código lo lee y lo procesa localmente en tu equipo. No se envía a ningún servidor en internet.</li>
                     <li className="gray-text"><span className="white-text">La Información se Desvanece:</span> Los resultados se generan y se te muestran a ti, y a nadie más. Una vez que cierras la pestaña del navegador, toda la información y los resultados se eliminan por completo.</li>
                 </ol>
@@ -34,7 +34,7 @@ return (
                     <li>No te rastreamos</li>
                 </ul>
             </div>
-            <p className="w-2xs gray-text my-20">Esta herramienta funciona como una <span className="blue-text">"caja de arena"</span> segura que se ejecuta en tu propio navegador. Eres la única persona que interactúa con tus datos de principio a fin. Tu privacidad no es solo una promesa, es el resultado del diseño de la aplicación.</p>
+            <p className="w-2xs gray-text my-20">Esta herramienta funciona como una <span className="blue-text">&quot;caja de arena&quot;</span> segura que se ejecuta en tu propio navegador. Eres la única persona que interactúa con tus datos de principio a fin. Tu privacidad no es solo una promesa, es el resultado del diseño de la aplicación.</p>
         </div>
     </>
 )
